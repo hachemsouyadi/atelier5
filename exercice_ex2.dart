c74fcee90c6ce126aa257b2e2663a97218a4b83e
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-11 21:21:34 -0500

    nouvelle modification

---
+++ exercice_ex2.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main(){
+stdout.write('entrez votre age :');
+var ageText = stdin.readLineSync();
+var age = int.parse(ageText!);
+try{
+  var age = int.parse(ageText!);
+} on FormatException catch (e){
+  print('format invalide : $e');
+}}
